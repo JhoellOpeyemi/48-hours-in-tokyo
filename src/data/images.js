2157a7ad2c3edc2782aa236f58798492e1d6c94b
Jhoell Opeyemi <75471937+JhoellOpeyemi@users.noreply.github.com>
2025-08-22 03:45:08 +0100

    fixed path name for images

--- src/data/images.js
+++ src/data/images.js
@@ -1,152 +1,152 @@
 export const images1 = [
   {
-    src: "/public/assets/tokyo-1.png",
+    src: "/assets/tokyo-1.png",
     alt: "",
   },
   {
-    src: "/public/assets/tokyo-2.png",
+    src: "/assets/tokyo-2.png",
     alt: "",
   },
   {
-    src: "/public/assets/tokyo-3.png",
+    src: "/assets/tokyo-3.png",
     alt: "",
   },
   {
-    src: "/public/assets/tokyo-4.png",
+    src: "/assets/tokyo-4.png",
     alt: "",
   },
   {
-    src: "/public/assets/tokyo-5.png",
+    src: "/assets/tokyo-5.png",
     alt: "",
   },
   {
-    src: "/public/assets/tokyo-1.png",
+    src: "/assets/tokyo-1.png",
     alt: "",
   },
   {
-    src: "/public/assets/tokyo-2.png",
+    src: "/assets/tokyo-2.png",
     alt: "",
   },
   {
-    src: "/public/assets/tokyo-3.png",
+    src: "/assets/tokyo-3.png",
     alt: "",
   },
   {
-    src: "/public/assets/tokyo-4.png",
+    src: "/assets/tokyo-4.png",
     alt: "",
   },
   {
-    src: "/public/assets/tokyo-5.png",
+    src: "/assets/tokyo-5.png",
     alt: "",
   },
   {
-    src: "/public/assets/tokyo-1.png",
+    src: "/assets/tokyo-1.png",
     alt: "",
   },
   {
-    src: "/public/assets/tokyo-2.png",
+    src: "/assets/tokyo-2.png",
     alt: "",
   },
 ];
 
 export const images2 = [
   {
-    src: "/public/assets/tokyo-6.png",
+    src: "/assets/tokyo-6.png",
     alt: "",
   },
   {
-    src: "/public/assets/tokyo-7.png",
+    src: "/assets/tokyo-7.png",
     alt: "",
   },
   {
-    src: "/public/assets/tokyo-8.png",
+    src: "/assets/tokyo-8.png",
     alt: "",
   },
   {
-    src: "/public/assets/tokyo-9.png",
+    src: "/assets/tokyo-9.png",
     alt: "",
   },
   {
-    src: "/public/assets/tokyo-10.png",
+    src: "/assets/tokyo-10.png",
     alt: "",
   },
   {
-    src: "/public/assets/tokyo-6.png",
+    src: "/assets/tokyo-6.png",
     alt: "",
   },
   {
-    src: "/public/assets/tokyo-7.png",
+    src: "/assets/tokyo-7.png",
     alt: "",
   },
   {
-    src: "/public/assets/tokyo-8.png",
+    src: "/assets/tokyo-8.png",
     alt: "",
   },
   {
-    src: "/public/assets/tokyo-9.png",
+    src: "/assets/tokyo-9.png",
     alt: "",
   },
   {
-    src: "/public/assets/tokyo-10.png",
+    src: "/assets/tokyo-10.png",
     alt: "",
   },
   {
-    src: "/public/assets/tokyo-6.png",
+    src: "/assets/tokyo-6.png",
     alt: "",
   },
   {
-    src: "/public/assets/tokyo-7.png",
+    src: "/assets/tokyo-7.png",
     alt: "",
   },
 ];
 
 export const mobileImages = [
   {
-    src: "/public/assets/tokyo-1.png",
+    src: "/assets/tokyo-1.png",
     alt: "",
   },
   {
-    src: "/public/assets/tokyo-2.png",
+    src: "/assets/tokyo-2.png",
     alt: "",
   },
   {
-    src: "/public/assets/tokyo-3.png",
+    src: "/assets/tokyo-3.png",
     alt: "",
   },
   {
-    src: "/public/assets/tokyo-4.png",
+    src: "/assets/tokyo-4.png",
     alt: "",
   },
   {
-    src: "/public/assets/tokyo-5.png",
+    src: "/assets/tokyo-5.png",
     alt: "",
   },
   {
-    src: "/public/assets/tokyo-6.png",
+    src: "/assets/tokyo-6.png",
     alt: "",
   },
   {
-    src: "/public/assets/tokyo-7.png",
+    src: "/assets/tokyo-7.png",
     alt: "",
   },
   {
-    src: "/public/assets/tokyo-8.png",
+    src: "/assets/tokyo-8.png",
     alt: "",
   },
   {
-    src: "/public/assets/tokyo-9.png",
+    src: "/assets/tokyo-9.png",
     alt: "",
   },
   {
-    src: "/public/assets/tokyo-10.png",
+    src: "/assets/tokyo-10.png",
     alt: "",
   },
   {
-    src: "/public/assets/tokyo-1.png",
+    src: "/assets/tokyo-1.png",
     alt: "",
   },
   {
-    src: "/public/assets/tokyo-2.png",
+    src: "/assets/tokyo-2.png",
     alt: "",
   },
 ];
